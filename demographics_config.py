482949b39602d741e2fa29fb12dd748e29a76d9e
Michael Brana <SerMike@users.noreply.github.com>
2024-11-02 17:07:55 -0400

    Merge local changes to remote

---
+++ demographics_config.py
@@ -0,0 +1,9 @@
+# Demographic Data
+AGE_GROUPS = ['0-17', '18-34', '35-49', '50-64', '65+']
+AGE_WEIGHTS = [0.15, 0.45, 0.25, 0.1, 0.05]
+
+SEX = ['Male', 'Female']
+SEX_WEIGHTS = [0.46, 0.54]
+
+GENDER_IDENTITY = ['Male', 'Female', 'Non-binary', 'Transgender Male', 'Transgender Female', 'Gender Fluid']
+GENDER_IDENTITY_WEIGHTS = [0.45, 0.47, 0.05, 0.01, 0.01, 0.01]
--- mutant_generator.py
+++ mutant_generator.py
@@ -1,29 +1,43 @@
 import random
 import numpy as np
 import pandas as pd
-from config import (ABILITIES, WEIGHTS, NUM_MUTANTS, CHUNK_SIZE, LANGUAGE_WEIGHTS, COUNTRIES, SEX,
-                    GENDER_IDENTITY, OCCUPATIONS, COUNTRY_WEIGHTS, OCCUPATION_WEIGHTS)
+from demographics_config import AGE_GROUPS, AGE_WEIGHTS, SEX, SEX_WEIGHTS, GENDER_IDENTITY, GENDER_IDENTITY_WEIGHTS
+from mutant_abilities_config import ABILITIES, WEIGHTS_DICT, WEIGHTS
+from countries_config import COUNTRIES, COUNTRY_WEIGHTS, LANGUAGE_WEIGHTS
+from occupations_config import OCCUPATIONS, OCCUPATION_WEIGHTS
 
+
+# Constants
+NUM_MUTANTS = 50000
+
+
+# Program runnning message...
 print("\nGenerating mutant population...")
 
 
-def generate_mutants_chunk(chunk_size, start_id):
-    country_list = list(COUNTRY_WEIGHTS.keys())
-    country_weights = list(COUNTRY_WEIGHTS.values())
-    age_groups = ['0-17', '18-34', '35-49', '50-64', '65+']
-    age_probabilities = [0.15, 0.45, 0.25, 0.1, 0.05]
-
+def generate_mutants(start_id):
     # Pre-generate random choices
-    countries = random.choices(country_list, weights=country_weights, k=chunk_size)
-    age_groups = np.random.choice(age_groups, size=chunk_size, p=age_probabilities)
-    sexes = random.choices(SEX, k=chunk_size)
-    gender_identities = random.choices(GENDER_IDENTITY, weights=[0.46, 0.46, 0.04, 0.02, 0.02, 0.005], k=chunk_size)
-    abilities = random.choices(ABILITIES, weights=WEIGHTS, k=chunk_size)
-    num_languages = np.random.choice([1, 2, 3, 4, 5], size=chunk_size, p=LANGUAGE_WEIGHTS)
-    occupations = random.choices(OCCUPATIONS, weights=OCCUPATION_WEIGHTS, k=chunk_size)
+        
+    age_groups = np.random.choice(AGE_GROUPS, p=AGE_WEIGHTS, size=NUM_MUTANTS)
+    sexes = np.random.choice(SEX, size=NUM_MUTANTS, p=SEX_WEIGHTS)
+    gender_identities = np.random.choice(GENDER_IDENTITY, size=NUM_MUTANTS, p=GENDER_IDENTITY_WEIGHTS)
+    num_languages = np.random.choice([1, 2, 3, 4, 5], size=NUM_MUTANTS, p=LANGUAGE_WEIGHTS)
+
+    total_abilities_weight = sum(WEIGHTS)
+    normalized_abilities_weights = [weight / total_abilities_weight for weight in WEIGHTS]
+    abilities = np.random.choice(ABILITIES, size=NUM_MUTANTS, p=normalized_abilities_weights)
+
+    total_occupations_weight = sum(OCCUPATION_WEIGHTS)
+    normalized_occupations_weights = [weight / total_occupations_weight for weight in OCCUPATION_WEIGHTS]
+    occupations = np.random.choice(OCCUPATIONS, size=NUM_MUTANTS, p=normalized_occupations_weights)
+
+    total_countries_weight = sum(COUNTRY_WEIGHTS.values())
+    normalized_countries_weights = [weight / total_countries_weight for weight in COUNTRY_WEIGHTS.values()]
+    countries = np.random.choice(list(COUNTRY_WEIGHTS.keys()), size=NUM_MUTANTS, p=normalized_countries_weights)
+
 
     data = []
-    for i in range(chunk_size):
+    for i in range(NUM_MUTANTS):
         mutant_id = start_id + i
         country = countries[i]
         age_group = age_groups[i]
@@ -44,18 +58,14 @@ def generate_mutants_chunk(chunk_size, start_id):
 
 
 def main():
-    chunks = []
-    for start_id in range(1, NUM_MUTANTS + 1, CHUNK_SIZE):
-        chunk = generate_mutants_chunk(min(CHUNK_SIZE, NUM_MUTANTS - start_id + 1), start_id)
-        chunks.append(chunk)
-
-    final_df = pd.concat(chunks, ignore_index=True)
+    # Generate all mutants
+    final_df = generate_mutants(start_id=1)
 
     file_path = 'mutant_population.csv'
     final_df.to_csv(file_path, index=False)
     print(f"\nData successfully saved to {file_path}"
           f"\nA total of {NUM_MUTANTS} mutants have been generated."
-          f"\n\nThat's a whole lotta mutants!")
+          f"\n\nThat's a whole lotta mutants!\n")
 
 
 if __name__ == "__main__":
